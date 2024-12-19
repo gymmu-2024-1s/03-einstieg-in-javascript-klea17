95b92b80a173472a3e3d3feb5e6e5e73fb165209
Klea Shima <e258777@edu.sbl.ch>
2024-12-19 18:31:47 +0000

    Aufgabe15 gelöst

--- script.js
+++ script.js
@@ -548,3 +548,24 @@ export function Bubblesort(args) {
 }
 
 linkupExerciseHandler("[data-click=Bubblesort]", Bubblesort)
+
+export function aufgabe15(args) {
+  const input = args
+  const result = []
+
+  let leerzeichenErkannt = false
+
+  for (let i = 0; i < input.length; i++) {
+    const currentElement = input[i]
+
+    if (currentElement === " ") {
+      leerzeichenErkannt = true
+    }
+
+    if (leerzeichenErkannt === false) {
+      result.push(currentElement)
+    }
+  }
+  return result.join("")
+}
+linkupExerciseHandler("[data-click=aufgabe15]", aufgabe15)
